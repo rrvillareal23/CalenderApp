631d95e39290bf67550cf5df5f6af5402b0d9c74
rrvillareal23 <rrvillareal@arizona.edu.com>
2024-05-12 13:17:36 -0700

    Initial Commit

---
+++ CalenderApp/CalenderAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalenderAppApp.swift
+//  CalenderApp
+//
+//  Created by Ricky Villareal on 5/12/24.
+//
+
+import SwiftUI
+
+@main
+struct CalenderAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
