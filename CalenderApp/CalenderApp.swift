d5e4994b07fc700eb23a653303bc1951998bb67d
rrvillareal23 <rrvillareal@arizona.edu.com>
2024-05-13 14:08:01 -0700

    First commit

--- CalenderApp/CalenderApp.swift
+++ CalenderApp/CalenderApp.swift
@@ -8,10 +8,10 @@
 import SwiftUI
 
 @main
-struct CalenderAppApp: App {
+struct CalenderApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MainView()
         }
     }
 }
